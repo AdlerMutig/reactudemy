89f75539bfa0ad9c1ed8c5a3c4cab0898e75391e
Paolo Aguilar <polsonbaggins@paolos-air.speedport.ip>
2020-03-16 21:19:48 +0100

    Filter news by keyword on search bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,14 @@
-import React, { Component } from "react";
-import "../css/styles.css";
+import React from "react";
+//import "../css/styles.css";
+import classes from "../css/styles.module.css";
 
-class Header extends Component {
-  state = {
-    keywords: ""
-  };
-  inputChangeHandler = event => {
-    this.setState({ keywords: event.target.value });
-  };
-  render() {
-    return (
-      <header className={this.state.active}>
-        <div className="logo">Logo</div>
-        <input type="text" onChange={this.inputChangeHandler} />
-      </header>
-    );
-  }
-}
+const Header = props => {
+  return (
+    <header className={classes.logo}>
+      <div className="logo">Logo</div>
+      <input type="text" onChange={props.keywords} />
+    </header>
+  );
+};
 
 export default Header;
